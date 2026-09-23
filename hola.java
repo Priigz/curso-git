9fa83a7aa82b830a295c2352ebdae27e46f6d709
Priscila Gimenez <priscilagimenez265@gmail.com>
2026-09-23 17:15:58 -0300

    fix : elimina un doble system

--- hola.java
+++ hola.java
@@ -3,7 +3,6 @@ public class hola {
     public static void main(String[] args) {
         String string = priscila;
         System.out.println(string);
-        System.out.println("Hola soy lau");
     
     }
     
